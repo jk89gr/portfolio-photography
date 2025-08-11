93e6b2e0f1de38f2f6f29a947ed816643af854c1
Dimitris Koutsodimos <jk89gr@gmail.com>
2025-08-11 16:24:10 +0300

    Fix Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-photography/',
+  base: '/portfolio-photography/', 
   build: {
-  outDir: 'docs'
-}
+    outDir: 'docs'
+  }
 })
-
